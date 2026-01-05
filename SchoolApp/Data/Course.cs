11a1b1e1bd32850f90af6591889f0fc396e6c018
thodkarik <thod.karik@gmail.com>
2026-01-05 17:15:56 +0200

    feat: add course entity

---
+++ SchoolApp/Data/Course.cs
@@ -0,0 +1,11 @@
+﻿namespace SchoolApp.Data
+{
+    public class Course : BaseEntity
+    {
+        public int Int { get; set; }
+        public string Description { get; set; } = null!;
+        public int? TeacherId { get; set; }
+        public virtual Teacher? Teacher { get; set; } = null!;
+        public virtual ICollection<Student> Students { get; set; } = new HashSet<Student>();
+    }
+}
